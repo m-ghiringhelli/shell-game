cc3346f9d147a2fa6584e73d6a887ab6028c4720
m-ghiringhelli <mghiring@gmail.com>
2022-01-24 16:52:38 -0800

    grab DOM elements

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 // import functions and grab DOM elements
+const ballOne = document.getElementById('ball-1');
+const ballTwo = document.getElementById('ball-2');
+const ballThree = document.getElementById('ball-3');
+
+
+const buttonOne = document.getElementById('button-1');
+const buttonTwo = document.getElementById('button-2');
+const buttonThree = document.getElementById('button-3');
+
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const messageEl = document.getElementById('trash-talk');
+const totalEl = document.getElementById('total');
 
 // let state
 
